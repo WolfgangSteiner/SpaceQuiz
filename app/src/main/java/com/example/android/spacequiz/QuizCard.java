380122bae1e6942c84c03daac037e3f9373a88a3
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2016-07-03 14:22:28 +0200

    initial commit

---
+++ app/src/main/java/com/example/android/spacequiz/QuizCard.java
@@ -0,0 +1,50 @@
+package com.example.android.spacequiz;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by wolfgang on 03.07.16.
+ */
+public class QuizCard extends LinearLayout
+{
+    ImageView mImageView;
+    TextView mQuestionView;
+    View mContentView;
+
+    public QuizCard(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.QuizCard,
+                0, 0);
+
+
+        LayoutInflater layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view=layoutInflater.inflate(R.layout.quiz_card,this);
+
+        Drawable imageDrawable = a.getDrawable(R.styleable.QuizCard_image);
+        String question = a.getString(R.styleable.QuizCard_question);
+
+
+        mImageView = (ImageView) findViewById(R.id.image_view);
+        mImageView.setImageDrawable(imageDrawable);
+
+        mQuestionView = (TextView) findViewById(R.id.question_view);
+        // mQuestionView.setText(question);
+    }
+
+    boolean IsAnswerCorrect()
+    {
+        return false;
+    }
+}
